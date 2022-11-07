63758bc66a909aa82aca3d579dba1568e3199dbe
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-11-07 17:16:25 -0300

    :sparkles: feat: validation-message component

---
+++ src/app/components/validation-message/validation-message.module.ts
@@ -0,0 +1,10 @@
+import { ValidationMessageComponent } from './validation-message.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [ValidationMessageComponent],
+  imports: [CommonModule],
+  exports: [ValidationMessageComponent],
+})
+export class ValidationMessageModule {}
