e5617f6607baf7a07aae55b8e2a7bc552c1d132e
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-11-08 23:48:22 -0300

    :sparkles: feat: email password reset component

--- src/app/auth/email-password-reset/email-password-reset.component.ts
+++ src/app/auth/email-password-reset/email-password-reset.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-email-password-reset',
   templateUrl: './email-password-reset.component.html',
-  styleUrls: ['./email-password-reset.component.scss']
+  styleUrls: ['./email-password-reset.component.scss'],
 })
 export class EmailPasswordResetComponent implements OnInit {
+  email!: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  sendPasswordResetLink() {
+    this.authService.sendPasswordResetLink(this.email);
   }
-
 }
--- src/app/auth/email-password-reset/email-password-reset.module.ts
+++ src/app/auth/email-password-reset/email-password-reset.module.ts
@@ -2,11 +2,15 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailPasswordResetComponent } from './email-password-reset.component';
+import { FormsModule } from '@angular/forms';
+import { ValidationMessageModule } from 'src/app/components/validation-message/validation-message.module';
 
 @NgModule({
   declarations: [EmailPasswordResetComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ValidationMessageModule,
     RouterModule.forChild([
       { path: '', component: EmailPasswordResetComponent },
     ]),
