23e6cf2965b03afcebafde70ab4d8305031d752e
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-11-08 23:48:08 -0300

    :sparkles: feat: email code component

--- src/app/auth/email-code/email-code.component.ts
+++ src/app/auth/email-code/email-code.component.ts
@@ -1,15 +1,32 @@
+import { AuthService } from './../auth.service';
+import { UserEmailCode } from './../../models/user-email-code';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-email-code',
   templateUrl: './email-code.component.html',
-  styleUrls: ['./email-code.component.scss']
+  styleUrls: ['./email-code.component.scss'],
 })
 export class EmailCodeComponent implements OnInit {
+  userEmailCode: UserEmailCode = {};
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.userEmailCode.token = params['token'];
+    });
   }
 
+  verify() {
+    this.authService.verifyEmailCode(this.userEmailCode);
+  }
+
+  resendConfirmationCode() {
+    this.authService.resendEmailCode(this.userEmailCode);
+  }
 }
--- src/app/auth/email-code/email-code.module.ts
+++ src/app/auth/email-code/email-code.module.ts
@@ -1,12 +1,16 @@
+import { ValidationMessageModule } from './../../components/validation-message/validation-message.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailCodeComponent } from './email-code.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EmailCodeComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ValidationMessageModule,
     RouterModule.forChild([{ path: '', component: EmailCodeComponent }]),
   ],
 })
