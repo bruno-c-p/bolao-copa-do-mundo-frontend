77cbd9e39ab06b8d97a07c4524fa0e699eb5cf92
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-11-10 22:44:15 -0300

    :sparkles: feat: ranking component

---
+++ src/app/models/user-ranking.ts
@@ -0,0 +1,4 @@
+export interface UserRanking {
+  nickname: string;
+  points: number;
+}
--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,15 +1,20 @@
+import { UserService } from './../services/user.service';
+import { UserRanking } from './../models/user-ranking';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
-  styleUrls: ['./ranking.component.scss']
+  styleUrls: ['./ranking.component.scss'],
 })
 export class RankingComponent implements OnInit {
+  users!: UserRanking[];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.ranking({ limit: 10 }).subscribe((users) => {
+      this.users = users;
+    });
   }
-
 }
