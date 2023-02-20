7bdace91a5175fd150428d4ffeaf6d7fed282961
Bruno Cardozo Pereira <bruno.cardozo.pereira@gmail.com>
2023-02-20 21:54:06 +0000

    :boom: fix: railway prod url fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  url_api: 'https://bolao-copa-do-mundo-api-production.up.railway.app/',
+  url_api: 'https://bolao-copa-do-mundo-api-production.up.railway.app',
   production: true,
 };
